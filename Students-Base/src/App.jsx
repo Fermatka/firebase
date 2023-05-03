6215e135f9324a772a6dc516b2960bd7d8c9bf53
Agata <agatka.fermatka@gmail.com>
2023-05-03 20:31:07 +0200

    working with firestore, students collection from fs rendered on page as a list

--- Students-Base/src/App.jsx
+++ Students-Base/src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import { db } from "./api/firebase";
+import { collection, onSnapshot, doc } from "firebase/firestore";
 
 console.log(db);
-function App() {
+export const App = () => {
+  const [Students, setStudents] = useState([]);
+  const studentsCollection = collection(db, "Students");
+  const getStudents = (querySnapshot) => {
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  };
+
+  useEffect(() => {
+    onSnapshot(studentsCollection, (querySnapshot) => {
+      const Students = getStudents(querySnapshot);
+      setStudents(Students);
+      console.log(Students);
+    });
+  }, []);
   return (
     <>
-      <h1>Hello Firebase!</h1>
+      <h1>Students list!</h1>
+      <ul>
+        {Students.map(({ id, firstName, lastName, age, yearOfStudies }) => (
+          <li data-id={id} id={id}>
+            <>
+              <p>Imię: {firstName}</p>
+              <p>Nazwisko:{lastName}</p>
+              <p>Wiek:{age}</p>
+              <p>Rok studiów:{yearOfStudies}</p>
+            </>
+          </li>
+        ))}
+      </ul>
     </>
   );
-}
-
-export default App;
+};
