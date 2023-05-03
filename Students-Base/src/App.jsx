13b13c8e8e94d299dd34923f97c40cbd2c56c1c3
Agata <agatka.fermatka@gmail.com>
2023-05-03 20:54:15 +0200

    adding students finished, connected with firestore working as well

--- Students-Base/src/App.jsx
+++ Students-Base/src/App.jsx
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
 import { db } from "./api/firebase";
-import { collection, onSnapshot, doc } from "firebase/firestore";
+import { collection, onSnapshot, addDoc } from "firebase/firestore";
 
 console.log(db);
 export const App = () => {
   const [Students, setStudents] = useState([]);
   const studentsCollection = collection(db, "Students");
+
   const getStudents = (querySnapshot) => {
     return querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
+  };
+
+  const getFormData = (event) => {
+    event.preventDefault();
+    const { firstName, lastName, age, yearOfStudies } =
+      event.currentTarget.elements;
+    const student = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      age: Number(age.value),
+      yearOfStudies: Number(yearOfStudies.value),
+    };
+    event.currentTarget.reset();
+    return student;
+  };
+
+  const handleSubmit = (event) => {
+    addDoc(studentsCollection, getFormData(event));
   };
 
   useEffect(() => {
@@ -22,15 +41,34 @@ export const App = () => {
   }, []);
   return (
     <>
-      <h1>Students list!</h1>
+      <h1>Students list</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">Podaj imię</label>
+          <input type="text" name="firstName" id="firstName"></input>
+        </div>
+        <div>
+          <label htmlFor="lastName">Podaj nazwisko</label>
+          <input type="text" name="lastName" id="lastName"></input>
+        </div>
+        <div>
+          <label htmlFor="age">Podaj wiek</label>
+          <input type="number" name="age" id="age"></input>
+        </div>
+        <div>
+          <label htmlFor="yearOfStudies">Podaj rok studiów </label>
+          <input type="number" name="yearOfStudies" id="yearOfStudies"></input>
+        </div>
+        <button>Dodaj studenta</button>
+      </form>
       <ul>
         {Students.map(({ id, firstName, lastName, age, yearOfStudies }) => (
           <li data-id={id} id={id}>
             <>
               <p>Imię: {firstName}</p>
-              <p>Nazwisko:{lastName}</p>
-              <p>Wiek:{age}</p>
-              <p>Rok studiów:{yearOfStudies}</p>
+              <p>Nazwisko: {lastName}</p>
+              <p>Wiek: {age}</p>
+              <p>Rok studiów: {yearOfStudies}</p>
             </>
           </li>
         ))}
